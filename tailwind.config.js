49e07fff42752154ca75b691c9c2be5efa854a96
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-01 13:36:47 -0500

    🚧  Add files and tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+      },
+      colors: {
+        desaturatedRed: "hsl(0, 36%, 70%)",
+        softRed: "hsl(0, 93%, 68%)",
+        darkGrayishRed: "hsl(0, 6%, 24%)",
+      },
+      backgroundImage: {
+        desktop: "url('./images/bg-pattern-desktop.svg)",
+      },
+    },
   },
   plugins: [],
-}
+};
