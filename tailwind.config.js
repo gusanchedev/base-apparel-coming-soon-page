d583161274515a47f695712a733d9ce74a04a6c6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-02-02 12:53:34 -0500

    💄  Add classes for mobile screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         desaturatedRed: "hsl(0, 36%, 70%)",
         softRed: "hsl(0, 93%, 68%)",
         darkGrayishRed: "hsl(0, 6%, 24%)",
+        initialColorGradient1: "hsl(0, 0%, 100%)",
+        finalColorGradient1: "hsl(0, 100%, 98%)",
+        initialColorGradient2: "hsl(0, 80%, 86%)",
+        finalColorGradient2: "hsl(0, 74%, 74%)",
       },
       backgroundImage: {
         desktop: "url('./images/bg-pattern-desktop.svg)",
